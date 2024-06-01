e759f2373668fcb79315cef18b839196905423d7
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2024-06-01 12:02:54 -0400

    Update trading.ts

--- src/utils/trading.ts
+++ src/utils/trading.ts
@@ -24,9 +24,14 @@ import { fromReadableAmount } from './conversions';
 import { getPoolInfo } from './pool';
 import { getProvider, sendTransaction, TransactionState } from './providers';
 
-const {
-  abi: SwapRouterABI,
-} = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json');
+const SwapRouterABI = [{"inputs":[{"internalType":"contract IPoolManager","name":"_manager","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"NoSwapOccurred","type":"error"},{"inputs":[],"name":"manager","outputs":[{"internalType":"contract IPoolManager","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"Currency","name":"currency0","type":"address"},{"internalType":"Currency","name":"currency1","type":"address"},{"internalType":"uint24","name":"fee","type":"uint24"},{"internalType":"int24","name":"tickSpacing","type":"int24"},{"internalType":"contract IHooks","name":"hooks","type":"address"}],"internalType":"struct PoolKey","name":"key","type":"tuple"},{"components":[{"internalType":"bool","name":"zeroForOne","type":"bool"},{"internalType":"int256","name":"amountSpecified","type":"int256"},{"internalType":"uint160","name":"sqrtPriceLimitX96","type":"uint160"}],"internalType":"struct IPoolManager.SwapParams","name":"params","type":"tuple"},{"components":[{"internalType":"bool","name":"takeClaims","type":"bool"},{"internalType":"bool","name":"settleUsingBurn","type":"bool"}],"internalType":"struct PoolSwapTest.TestSettings","name":"testSettings","type":"tuple"},{"internalType":"bytes","name":"hookData","type":"bytes"}],"name":"swap","outputs":[{"internalType":"BalanceDelta","name":"delta","type":"int256"}],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"bytes","name":"rawData","type":"bytes"}],"name":"unlockCallback","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"nonpayable","type":"function"}];
+
+// You can now use SwapRouterABI in your ethers.Contract call
+const swapRouterContract = new ethers.Contract(
+  SWAP_ROUTER_ADDRESS,
+  SwapRouterABI,
+  ethProvider as ethers.providers.Provider
+);
 
 export type TokenTrade = Trade<Token, Token, TradeType>;
 
